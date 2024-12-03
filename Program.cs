76af87b01eaa5afa8186a92ed3ce0cd6e92026bc
jbiggs512 <jack.biggs8451@gmail.com>
2024-12-03 14:40:15 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Sorting_with_menus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+    }
+
+    class FileManager
+    {
+        List <int> numbers = new List<int>();
+
+        List<string> fileContents = File.ReadAllLines('../');
+
+
+
+    }
+}
