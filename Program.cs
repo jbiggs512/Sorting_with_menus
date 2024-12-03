c2c764884bcd64a1d78b85340bb5e25bbcbd5918
jbiggs512 <jack.biggs8451@gmail.com>
2024-12-03 15:35:42 +0000

    Added read csv to integer list

--- Program.cs
+++ Program.cs
@@ -8,18 +8,33 @@ namespace Sorting_with_menus
     {
         static void Main(string[] args)
         {
+            string filePath = "../../../NumSets/NumSet_Small.csv";
             Console.WriteLine("Hello, World!");
+
+            if (File.Exists(filePath))
+            {
+                var numbers = File.ReadLines(filePath)
+                    .Where((line) => !line.StartsWith('#') && !string.IsNullOrWhiteSpace(line)).ToList()
+                    .SelectMany((line) => line.Split(','))
+                    .Select((number) => int.Parse(number)).ToList();
+
+                // For testing!!!
+                foreach (var number in numbers) {
+                    Console.WriteLine(number);
+                }
+            }
+
+
+
+
+
+
         }
 
     }
 
     class FileManager
     {
-        List <int> numbers = new List<int>();
-
-        List<string> fileContents = File.ReadAllLines('../');
-
-
-
+       
     }
 }
